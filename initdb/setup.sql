4989435922f397f7463fa57bf8d968e4fb8daf62
Robson Santos <robfds@gmail.com>
2021-07-25 12:13:54 -0300

    Solution for node challenge

---
+++ initdb/setup.sql
@@ -0,0 +1,7 @@
+USE nodedb;
+
+CREATE TABLE people(
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255),
+  PRIMARY KEY(id)
+);
---
+++ node/index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const config = {
+	host: 'db_challenge',
+	user: 'root',
+	password: 'root',
+	database: 'nodedb',
+};
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+const sql_insert = `INSERT INTO people(name) VALUES('Robson')`
+connection.query(sql_insert)
+var results
+connection.query("SELECT id, name FROM people", function (err, result, fields) {
+	results = result
+})
+
+connection.end()
+
+function foo () {
+	let str;
+	let names = results.map(function(k) {
+		return `<p><strong>${k.id}</strong>: ${k.name}</p>`
+	})
+	return names.join('\n')
+}
+
+app.get('/', (req,res) => {
+	res.send(
+		`<h1>FullCycle Challenge</h1>
+		<h3>Using docker-compose to launch a dummy application with nodejs + nginx + mysql</h3>
+		<p>${foo()}</p>
+		`
+	)
+})
+
+app.listen(port, () => {
+	console.log('Rodando na porta ' + port)
+})
